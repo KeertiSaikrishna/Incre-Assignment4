92d0a4b0ad3d3aec3ff3685e6496a4a9d7d6330a
KeertiSaikrishna <31760338+KeertiSaikrishna@users.noreply.github.com>
2018-08-17 10:35:52 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,23 +1,16 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Main {
     public static void main(String[] args) {
-        try{
-            Main obj = new Main();
-            obj.throwexc(1);
-        }
-        catch(NullPointerException | IndexOutOfBoundsException | ArithmeticException exc){
-            System.out.println(exc);
-        }
-        finally {
-            System.out.println("finally block");
-        }
-    }
+        String str = "This is simple example to check wheather a sentence starts with capital letter and ends with a period.";
+        Pattern p = Pattern.compile("^[A-Z].*\\.$");
+        Matcher m = p.matcher(str);
 
-    void throwexc(int i){
-        if(i==1)
-            throw new NullPointerException();
-        else if (i==2)
-            throw new IndexOutOfBoundsException();
-        else
-            throw new ArithmeticException();
+        while (m.find()) {
+            System.out.print("Start index: " + m.start());
+            System.out.print(" End index: " + m.end() + "\n ");
+            System.out.println(m.group());
+        }
     }
 }
